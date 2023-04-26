9589e285092e1279c18756a085aa536f8b5f63fa
David Garay <garaekz@gmail.com>
2023-04-26 13:52:56 -0700

    🎬 Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./index.html",
+    "./src/**/*.{ts}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
